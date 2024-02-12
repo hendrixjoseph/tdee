af50d25cb1d659f5cae1fdeb5d846a58f99018ef
hendrixjoseph <hendixjoseph@aol.com>
2024-02-12 15:01:52 -0500

    show by day of week

--- src/main/java/com/joehxblog/tdee/Main.java
+++ src/main/java/com/joehxblog/tdee/Main.java
@@ -1,7 +1,6 @@
 package com.joehxblog.tdee;
 
 import com.joehxblog.converter.Converters;
-import com.joehxblog.converter.LocalDateConverter;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriterBuilder;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -12,6 +11,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -33,17 +33,42 @@ public class Main {
 
         var weightChangeEntries = new WeightChangeEntryBuilder().with(diaryEntries);
 
-
-
+        writeEnergyExpenditures(weightChangeEntries);
+        compareGainVsLoss(weightChangeEntries);
+        byDayOfWeek(weightChangeEntries);
     }
 
-    private static void compareGainVsLoss(List<WeightChangeEntry> weightChangeEntries) throws IOException {
-        var groupings = weightChangeEntries.stream().collect(
+    private static void byDayOfWeek(List<WeightChangeEntry> weightChangeEntries) throws IOException {
+        var dayOfWeekMap = weightChangeEntries.stream().collect(
+                Collectors.groupingBy(
+                    w -> w.date().getDayOfWeek(),
+                    Collectors.partitioningBy(w -> w.changeInWeight() > 0)
+                )
+        );
+
+        var list = dayOfWeekMap.entrySet().stream().map(e -> {
+            var dayOfWeek = e.getKey().toString();
+            var gainCount = Integer.toString(e.getValue().get(true).size());
+            var lossCount = Integer.toString(e.getValue().get(false).size());
+
+            return new String[] {dayOfWeek, gainCount, lossCount};
+        })
+        .toList();
+
+        write(list, "byDayOfWeek.csv");
+    }
+
+    private static Map<Boolean, List<Integer>> groupWeightChangeEntries(List<WeightChangeEntry> weightChangeEntries) {
+        return weightChangeEntries.stream().collect(
                 Collectors.partitioningBy(
                         e -> e.changeInWeight() > 0,
                         Collectors.mapping(WeightChangeEntry::dailyNet, Collectors.toList())
                 )
         );
+    }
+
+    private static void compareGainVsLoss(List<WeightChangeEntry> weightChangeEntries) throws IOException {
+        var groupings = groupWeightChangeEntries(weightChangeEntries);
 
         var gainedWeight = groupings.get(true);
         var lostWeight = groupings.get(false);
@@ -68,7 +93,9 @@ public class Main {
         }
     }
 
-    private static void writeEnergyExpenditures(List<EnergyExpenditure> energyExpenditures) throws IOException {
+    private static void writeEnergyExpenditures(List<WeightChangeEntry> weightChangeEntries) throws IOException {
+        var energyExpenditures = new EnergyExpenditureBuilder().with(weightChangeEntries);
+
         var list = energyExpenditures.stream()
                 .map(e -> {
                     var date = Converters.get(LocalDate.class).toString(e.date());
